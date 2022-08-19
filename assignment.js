66bfebb4c179946859a494a7d4ff90c03596f8eb
farhana <farhana17mar@gmail.com>
2022-08-20 02:49:10 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,81 @@
+//problem number 1
+
+// function radianToDegree(degree) {
+//     const radianValue = 57.2958;
+//     let result = degree * radianValue;
+//     return result;
+// }
+
+// let finalResult = radianToDegree(25);
+// console.log(finalResult.toFixed(2));
+
+
+
+//problem 2
+
+// function isJavaScriptFile(string) {
+//     if (string.includes('.js')) {
+//         return true;
+//     } else {
+//         return false
+//     }
+// }
+
+// let stringValue = 'app.png';
+// let finalResult1 = isJavaScriptFile(stringValue);
+// console.log(finalResult1);
+
+
+// problem 3
+
+// function oilPrice(num1, num2, num3) {
+//     const dieselPrice = 114;
+//     const petrolPrice = 130;
+//     const octanePrice = 135;
+//     let dieselPriceTotal = num1 * dieselPrice;
+//     let petrolPriceTotal = num2 * petrolPrice;
+//     let octantPriceTotal = num3 * octanePrice;
+//     let totalBill = dieselPriceTotal + petrolPriceTotal + octantPriceTotal;
+//     return totalBill;
+// }
+
+// let carNeedsBill = oilPrice(3, 7, 9);
+// console.log(carNeedsBill);
+
+
+//problem 4
+
+// function publicBusFare(peopleNumber) {
+//     const reservedBus = 50;
+//     const microBus = 11;
+//     let publicBus = peopleNumber - reservedBus - microBus;
+//     let publicBusFare = 250 * publicBus;
+//     return publicBusFare;
+// }
+
+// let finalResult4 = publicBusFare(62);
+// console.log(finalResult4);
+
+
+// problem 5
+// function isBestFriend(friend1, friend2) {
+//     if (friend1.name == friend2.friend && friend2.name == friend1.friend) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+
+// }
+
+// let friend1 = {
+//     name: "farhana",
+//     friend: 'sabil'
+// };
+
+// let friend2 = {
+//     name: 'Zarif',
+//     friend: 'farhana'
+// };
+
+// let result5 = isBestFriend(friend1, friend2);
+// console.log(result5);
